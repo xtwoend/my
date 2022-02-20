1237061a05d14d679537fd815ce9a2b05808455e
Abdul Hafidz A <aditans88@gmail.com>
2022-02-20 23:03:11 +0700

    c

---
+++ app/Models/Sync.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sync extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['location_id', 'date', 'status', 'trying', 'response'];
+}

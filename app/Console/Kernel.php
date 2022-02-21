790543dec09cb5fafa58311f1745178c89d8bd7e
Abdul Hafidz A <aditans88@gmail.com>
2022-02-21 07:22:50 +0700

    c

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
+use App\Console\Commands\SyncCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +17,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $fastDay = Carbon::now()->subDay()->format('Y-m-d');
         $schedule->command('attendance:schedule:generator')->weeklyOn(1, '00:00');
+        $schedule->command(SyncCommand::class, [$fastDay])->dailyAt('07:22');
     }
 
     /**
